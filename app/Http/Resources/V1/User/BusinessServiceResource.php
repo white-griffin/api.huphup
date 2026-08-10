b8b9c69b56198187affdc0e1e0ccb7562959d83d
Amin <mohamadamin.zanguee@gmail.com>
2026-08-10 15:38:48 +0330

    add service_id to services in business service resource

--- app/Http/Resources/V1/User/BusinessServiceResource.php
+++ app/Http/Resources/V1/User/BusinessServiceResource.php
@@ -12,6 +12,7 @@ class BusinessServiceResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->service->name,
             'description' => $this->service->description,
             'icon' => $this->service->icon,
